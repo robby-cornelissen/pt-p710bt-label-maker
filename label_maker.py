036da039881e7825b75b5990f05aa080337e10fd
Robby Cornelissen <robby.cornelissen@gmail.com>
2020-03-20 23:54:55 +0900

    Finish basic implementation

--- label_maker.py
+++ label_maker.py
@@ -1,7 +1,8 @@
-from enum import Flag
+from enum import IntEnum, IntFlag
 import sys
 import contextlib
 import bluetooth
+from label_rasterizer import encode_png, rasterize
 
 
 STATUS_OFFSET_ERROR_INFORMATION_1 = 8
@@ -19,20 +20,20 @@ STATUS_OFFSET_TEXT_COLOR_INFORMATION = 25
 STATUS_OFFSET_HARDWARE_SETTINGS = 26
 
 
-class ErrorInformation1(Flag):
+class ErrorInformation1(IntFlag):
     NO_MEDIA = 0x01
     CUTTER_JAM = 0x04
     WEAK_BATTERIES = 0x08
     HIGH_VOLTAGE_ADAPTER = 0x40
 
 
-class ErrorInformation2(Flag):
+class ErrorInformation2(IntFlag):
     WRONG_MEDIA = 0x01
     COVER_OPEN = 0x10
     OVERHEATING = 0x20
 
 
-class MediaType(Flag):
+class MediaType(IntEnum):
     NO_MEDIA = 0x00
     LAMINATED_TAPE = 0x01
     NON_LAMINATED_TAPE = 0x03
@@ -40,7 +41,12 @@ class MediaType(Flag):
     INCOMPATIBLE_TAPE = 0xFF
 
 
-class StatusType(Flag):
+class Mode(IntFlag):
+    AUTO_CUT = 0x40
+    MIRROR_PRITING = 0x80
+
+
+class StatusType(IntEnum):
     REPLY_TO_STATUS_REQUEST = 0x00
     PRINTING_COMPLETED = 0x01
     ERROR_OCCURRED = 0x02
@@ -49,26 +55,27 @@ class StatusType(Flag):
     PHASE_CHANGE = 0x06
 
 
-class PhaseType(Flag):
+class PhaseType(IntEnum):
     EDITING_STATE = 0x00
     PRINTING_STATE = 0x01
 
 
-class PhaseNumberEditingState(Flag):
+class PhaseNumberEditingState(IntEnum):
     EDITING_STATE = 0x0000
     FEED = 0x0001
 
-class PhaseNumberPrintingState(Flag):
+class PhaseNumberPrintingState(IntEnum):
     PRINTING = 0x0000
     COVER_OPEN_WHILE_RECEIVING = 0x0014
 
 
-class NotificationNumber(Flag):
+class NotificationNumber(IntEnum):
+    NOT_AVAILABLE = 0x00
     COVER_OPEN = 0x01
     COVER_CLOSED = 0x02
 
 
-class TapeColor(Flag):
+class TapeColor(IntEnum):
     WHITE = 0x01
     OTHER = 0x02
     CLEAR = 0x03
@@ -101,7 +108,7 @@ class TapeColor(Flag):
     INCOMPATIBLE = 0xFF
 
 
-class TextColor(Flag):
+class TextColor(IntEnum):
     WHITE = 0x01
     OTHER = 0x02
     RED = 0x04
@@ -124,6 +131,8 @@ def bt_socket_manager(*args, **kwargs):
 
 
 def make_label(image_path, bt_address, bt_channel):
+    data = encode_png(image_path)
+
     with bt_socket_manager(bluetooth.RFCOMM) as socket:
         socket.connect((bt_address, bt_channel))
 
@@ -131,8 +140,21 @@ def make_label(image_path, bt_address, bt_channel):
         send_initialize(socket)
         send_status_information_request(socket)
 
-        receive_status_information_response(socket)
+        status_information = receive_status_information_response(socket)
+        handle_status_information(status_information)
 
+        send_switch_dynamic_command_mode(socket)
+        send_switch_automatic_status_notification_mode(socket)
+        send_print_information_command(socket, data)
+        send_various_mode_settings(socket)
+        send_advanced_mode_settings(socket)
+        send_specify_margin_amount(socket)
+        send_select_compression_mode(socket)
+        send_raster_data(socket, data)
+        send_print_command_with_feeding(socket)
+
+        status_information = receive_status_information_response(socket)
+        handle_status_information(status_information)
 
 
 def send_invalidate(socket):
@@ -151,8 +173,8 @@ def send_switch_dynamic_command_mode(socket):
 
 
 def send_switch_automatic_status_notification_mode(socket):
-    # set automatic status notification mode to "do not notify" [1B 69 21 {01}]
-    socket.send(b"\x1B\x69\x21\x01")
+    # set automatic status notification mode to "notify" [1B 69 21 {00}]
+    socket.send(b"\x1B\x69\x21\x00")
 
 
 def send_print_information_command(socket, data):
@@ -164,7 +186,8 @@ def send_print_information_command(socket, data):
 
 def send_various_mode_settings(socket):
     # set to auto-cut, no mirror printing [1B 69 4D {40}]
-    socket.send(b"\x1B\x69\x4D\x40")
+    socket.send(b"\x1B\x69\x4D")
+    socket.send(Mode.AUTO_CUT.to_bytes(1, "big"))
 
 
 def send_advanced_mode_settings(socket):
@@ -182,8 +205,10 @@ def send_select_compression_mode(socket):
     socket.send(b"\x4D\x02")
 
 
-def send_raster_data(socket):
-    pass
+def send_raster_data(socket, data):
+    # send all raster data lines
+    for line in rasterize(data):
+        socket.send(bytes(line))
 
 
 def send_print_command_with_feeding(socket):
@@ -201,18 +226,59 @@ def receive_status_information_response(socket):
     response = socket.recv(32)
 
     if (len(response) != 32):
-        sys.exit("Expected 32 bytes, but only received [%d]" % len(response))
+        sys.exit("Expected 32 bytes, but only received %d" % len(response))
 
-    print(response.hex())
-
-
-def print_status_information(status_information):
-    pass
+    return response
 
 
-def check_status_information_for_error(status_information):
-    pass
+def handle_status_information(status_information):
+    def handle_reply_to_status_request(status_information):
+        print("Media width: %dmm" % status_information[STATUS_OFFSET_MEDIA_WIDTH])    
+        print("Media type: %s" % MediaType(status_information[STATUS_OFFSET_MEDIA_TYPE]))
+        print("Tape color information: %s" % TapeColor(status_information[STATUS_OFFSET_TAPE_COLOR_INFORMATION]).name)
+        print("Text color information: %s" % TextColor(status_information[STATUS_OFFSET_TEXT_COLOR_INFORMATION]).name)
 
+    def handle_printing_completed(status_information):
+        mode = Mode(status_information[STATUS_OFFSET_MODE])
+
+        print("Mode: %s" % ", ".join([f.name for f in Mode if f in mode]))
+
+        sys.exit(0)
+
+    def handle_error_occurred(status_information):
+        error_information_1 = ErrorInformation1(status_information[STATUS_OFFSET_ERROR_INFORMATION_1])
+        error_information_2 = ErrorInformation1(status_information[STATUS_OFFSET_ERROR_INFORMATION_2])
+
+        print("Error information 1: %s" % ", ".join([f.name for f in ErrorInformation1 if f in error_information_1]))
+        print("Error information 2: %s" % ", ".join([f.name for f in ErrorInformation2 if f in error_information_2]))
+
+        sys.exit("An error occurred")
+
+    def handle_turned_off(status_information):
+        sys.exit("Device was turned off")
+
+    def handle_notification(status_information):
+        print("Notification number: %s" % NotificationNumber(status_information[STATUS_OFFSET_NOTIFICATION_NUMBER]).name)
+
+    def handle_phase_change(status_information):
+        phase_type = status_information[STATUS_OFFSET_PHASE_TYPE]
+        phase_number = int.from_bytes(status_information[STATUS_OFFSET_PHASE_NUMBER:STATUS_OFFSET_PHASE_NUMBER + 2], "big")
+
+        print("Phase type: %s" % PhaseType(phase_type).name)
+        print("Phase number: %s" % (PhaseNumberPrintingState(phase_number) if phase_type == PhaseType.PRINTING_STATE else PhaseNumberEditingState(phase_number)).name)
+
+    handlers = {
+        StatusType.REPLY_TO_STATUS_REQUEST: handle_reply_to_status_request,
+        StatusType.PRINTING_COMPLETED: handle_printing_completed,
+        StatusType.ERROR_OCCURRED: handle_error_occurred,
+        StatusType.TURNED_OFF: handle_turned_off,
+        StatusType.NOTIFICATION: handle_notification,
+        StatusType.PHASE_CHANGE: handle_phase_change
+    }
+
+    status_type = status_information[STATUS_OFFSET_STATUS_TYPE]
+
+    handlers[status_type](status_information)
 
 def main(*args):
     if len(args) < 3:
---
+++ label_rasterizer.py
@@ -0,0 +1,54 @@
+import sys
+import png
+import packbits
+
+IMAGE_HEIGHT = 128
+CHUNK_SIZE = 16
+ZERO_CHUNK = bytearray(b"\x00" * CHUNK_SIZE)
+RASTER_COMMAND = b"\x47"
+ZERO_COMMAND = b"\x5A"
+
+
+def rasterize(encoded_image_data):
+    for i in range(0, len(encoded_image_data), CHUNK_SIZE):
+        buffer = bytearray()
+        chunk = encoded_image_data[i:i + CHUNK_SIZE]
+        
+        if chunk == ZERO_CHUNK:
+            buffer += ZERO_COMMAND
+        else:
+            packed_chunk = packbits.encode(chunk)
+
+            buffer += RASTER_COMMAND
+            buffer += len(packed_chunk).to_bytes(2, "little")
+            buffer += packbits.encode(chunk)
+        
+        yield buffer
+
+
+def encode_png(image_path):
+    width, height, rows, info = png.Reader(filename=image_path).asRGBA()
+
+    if height != IMAGE_HEIGHT:
+        sys.exit("Image height is %d pixels, %d required" % (height, IMAGE_HEIGHT))
+
+    # get all the alpha channel values, rotate 90 degrees and flip horizontally
+    data = [row[3::4] for row in rows]
+    data = list(zip(*data))
+
+    buffer = bytearray()
+
+    for line in data:
+        # create one byte for every 8 values
+        for i in range(0, len(line), 8):
+            byte = 0;
+
+            for j in range(0, 8):
+                value = line[i + j]
+                
+                if value > 0:
+                    byte |= (1 << (7 - j))
+            
+            buffer.append(byte)
+
+    return buffer
